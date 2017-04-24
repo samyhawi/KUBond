469962faaa57d663585b8a5fdfef4fbfd8900587
samyhawi <s.hawi@live.com>
2017-04-24 20:20:20 +0100

    Update ContactsViewController.swift
    Deleted comments

--- BondKU/ContactsViewController.swift
+++ BondKU/ContactsViewController.swift
@@ -34,13 +34,9 @@ class ContactsViewController: UITableViewController{
             if let dictionary = snapshot.value as? [String: Any] {
                 let user = UserDatabase(dic: dictionary)
                 
-                //if you use this setter, your app will crash if your class properties don't exactly match up with the firebase dictionary keys
-                self.users.append(user)
+                 self.users.append(user)
                 
-                //this will crash because of background thread, so lets use dispatch_async to fix
-                
-                
-                //                user.name = dictionary["name"]
+             
             }
         }, withCancel: nil)
     }
@@ -53,8 +49,7 @@ class ContactsViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //  let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        
+      
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         
         let user = users[indexPath.row]
