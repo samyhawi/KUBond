6f440d793317f67ea0e5240ccf291f0855ef8b82
samyhawi <s.hawi@live.com>
2017-04-24 20:18:25 +0100

    Update HomepageViewController.swift
    Deleted comments

--- BondKU/HomepageViewController.swift
+++ BondKU/HomepageViewController.swift
@@ -76,7 +76,7 @@ extension HomepageViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //  let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+       
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostsTableViewCellIdentifier", for: indexPath) as? PostsTableViewCell
         if let post = posts[indexPath.row] as? Post{
             cell?.setUpCell(post: post)
@@ -86,100 +86,3 @@ extension HomepageViewController: UITableViewDelegate, UITableViewDataSource{
     }
 
 }
-
-
-
-/*
- import UIKit
- import Firebase
- import FirebaseDatabaseUI
- import FirebaseAuthUI
- 
- @objc(PostViewController)
- class PostViewController: UIViewController, UITableViewDelegate {
- 
- 
- 
- // [START define_database_reference]
- var ref: FIRDatabaseReference!
- // [END define_database_reference]
- 
- var dataSource: FUITableViewDataSource
- 
- @IBOutlet weak var tableView: UITableView!
- 
- override func viewDidLoad() {
- super.viewDidLoad()
- 
- // [START create_database_reference]
- ref = FIRDatabase.database().reference()
- // [END create_database_reference]
- 
- dataSource = FirebaseTableViewDataSource.init(query: getQuery(),
- modelClass: Post.self,
- nibNamed: "PostTableViewCell",
- cellReuseIdentifier: "post",
- view: self.tableView)
- 
- dataSource.populateCell() {
- guard let cell = $0 as? PostTableViewCell else {
- return
- }
- guard let post = $1 as? Post else {
- return
- }
- cell.authorImage.image = UIImage.init(named: "ic_account_circle")
- cell.authorLabel.text = post.author
- var imageName = "ic_star_border"
- if (post.stars?[self.getUid()]) != nil {
- imageName = "ic_star"
- }
- cell.starButton.setImage(UIImage.init(named: imageName), for: .normal)
- if let starCount = post.starCount {
- cell.numStarsLabel.text = "\(starCount)"
- }
- cell.postTitle.text = post.title
- cell.postBody.text = post.body
- }
- 
- tableView.dataSource = dataSource
- tableView.delegate = self
- }
- 
- override func viewWillAppear(_ animated: Bool) {
- self.tableView.reloadData()
- }
- 
- func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
- performSegue(withIdentifier: "detail", sender: indexPath)
- }
- 
- func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
- return 150
- }
- 
- func getUid() -> String {
- return (FIRAuth.auth()?.currentUser?.uid)!
- }
- 
- func getQuery() -> FIRDatabaseQuery {
- return self.ref
- }
- 
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- guard let path: IndexPath = sender as? IndexPath else { return }
- guard let detail: PostDetailTableViewController = segue.destination as? PostDetailTableViewController else {
- return
- }
- let source = self.dataSource
- guard let snapshot: FIRDataSnapshot = (source.object(at: UInt((path as NSIndexPath).row)))! as? FIRDataSnapshot else {
- return
- }
- detail.postKey = snapshot.key
- }
- 
- override func viewWillDisappear(_ animated: Bool) {
- getQuery().removeAllObservers()
- }
- 
- }*/
